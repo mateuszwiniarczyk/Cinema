0c8b84171b038fccfe71e8e4f669a2e2471bf48e
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2021-07-08 20:58:25 +0200

    Create global theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
+import { theme } from 'assets/styles/theme';
 
 const App = () => (
-  <>
+  <ThemeProvider theme={theme}>
     <GlobalStyle />
     <h1>Test</h1>
-  </>
+  </ThemeProvider>
 );
 
 export default App;
