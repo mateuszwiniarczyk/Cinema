1e0a8d0407ea6eed52457767e5e42a0767705bc7
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2021-07-16 19:51:14 +0200

    Create test for sign in form

---
+++ src/__tests__/SignIn.tsx
@@ -0,0 +1,12 @@
+import SignIn from 'pages/SignIn';
+import { screen } from '@testing-library/react';
+import { renderWithProviders } from 'helpers/renderWithProviders';
+
+describe('<SignIn />', () => {
+  it('should display a link to the login page', () => {
+    renderWithProviders(<SignIn />);
+    const link = screen.getByRole('link', { name: 'Sign Up' });
+
+    expect(link).toHaveAttribute('href', '/signup');
+  });
+});
