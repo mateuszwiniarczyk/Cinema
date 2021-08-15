16f93d37b1af87a7a772f45c0dcfd0129904f214
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2021-08-15 21:55:35 +0200

    Fix build

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -7,19 +7,37 @@ import { Link } from 'react-router-dom';
 import useTrendingMedia from 'hooks/useTrendingMedia';
 import Loader from 'components/Loader';
 
+type movie = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  media_type: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
 const Hero = (): JSX.Element => {
   const { isLoading, isError, trendingMedia } = useTrendingMedia({
     type: 'movie',
     timeRange: 'week'
   });
 
-  const mainMedia = trendingMedia?.length ? trendingMedia[0] : null;
+  const mainMedia = trendingMedia?.length ? (trendingMedia[0] as movie) : null;
 
   return (
     <Wrapper>
       {mainMedia && !isLoading ? (
         <Container>
-          <Image src={`https://image.tmdb.org/t/p/w1280/${mainMedia.backdrop_path}`} alt="" />
+          <Image src={`https://image.tmdb.org/t/p/w1280/${backdrop_path}`} alt="" />
           <Content>
             <Title>{mainMedia.title}</Title>
             <DetailsList
